93914d51c84046fae0036083bfeee04f2e373580
Paula Rösler <paularoesler@web.de>
2021-02-10 14:24:52 +0100

    Minor corrections

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import getCharacters from './services/getCharacters'
 
 export default function App() {
   const header = AppHeader('Harry Potter App')
-  document.body.append('header')
+  document.body.append(header)
 }
 
 // fetch API
