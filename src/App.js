2cd846e50e4001e1a54a617c27364a35d7c4312d
Paula Rösler <paularoesler@web.de>
2021-02-10 15:21:14 +0100

    Implementing Cards first step

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import AppHeader from './components/AppHeader'
 import getCharacters from './services/getCharacters'
+import Card from './components/Card'
+import createElement from './lib/createElement'
 
 export default function App() {
   const header = AppHeader('Harry Potter App')
   document.body.append(header)
-}
 
-// fetch API
-getCharacters()
-  .then(data => console.log(data))
-  .chatch(error => console.log(error))
+  // fetch API
+  getCharacters()
+    .then(characters => createCards(characters))
+    .catch(error => handleGetCharacterError(error))
+
+  function createCards(characters) {
+    const cards = characters.map(character => Card(character.name))
+    document.body.append(...cards)
+  }
+
+  function handleGetCharacterError(error) {
+    const errorMessage = createElement(
+      'strong',
+      { style: 'color: crimson;' },
+      error.message
+    )
+    document.body.append(errorMessage)
+  }
+}
