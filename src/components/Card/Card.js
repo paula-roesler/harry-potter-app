9e87011dd7305b17a2ab44ecf3e081ab13300687
Paula Rösler <paularoesler@web.de>
2021-02-11 06:44:09 +0100

    Add color from house names

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -28,10 +28,33 @@ export default function Card(image, name, house, ancestry, patronus) {
   const el = createElement(
     'section',
     { className: 'Card' },
-    characterName,
     profilePicture,
+    characterName,
     characterDetails
   )
+
+  function houseColors(house) {
+    let houseColor
+
+    if (house === 'Gryffindor') {
+      houseColor = el.style.background = '#cf1e2e'
+    }
+    if (house === 'Ravenclaw') {
+      el.style.backgroundColor = '#4b90cb'
+    }
+    if (house === 'Hufflepuff') {
+      el.style.backgroundColor = '#ffcf47'
+    }
+    if (house === 'Slytherin') {
+      el.style.backgroundColor = '#016241'
+    }
+    if (house === '') {
+      el.style.backgroundColor = 'grey'
+    }
+    return houseColor
+  }
+
+  houseColors(house)
 
   return el
 }
